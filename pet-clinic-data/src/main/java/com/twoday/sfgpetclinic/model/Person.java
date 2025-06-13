52fb3d732f32006603b74b6a4389917897e0e461
Lauri Heinonen <lauri.heinonen@twoday.com>
2025-06-13 16:05:40 +0300

    refactored into multi-module maven application (separate data and web modules) Added lombok getters & setters

---
+++ pet-clinic-data/src/main/java/com/twoday/sfgpetclinic/model/Person.java
@@ -0,0 +1,13 @@
+package com.twoday.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+}
---
+++ pet-clinic-data/src/main/java/com/twoday/sfgpetclinic/model/PetType.java
@@ -0,0 +1,12 @@
+package com.twoday.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PetType {
+
+    private String name;
+
+}
