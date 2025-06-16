418154b636127e6a39e7967e1e8f6220a80bca76
Lauri Heinonen <lauri.heinonen@twoday.com>
2025-06-16 13:08:31 +0300

    add generated ids for objects

--- pet-clinic-web/src/main/java/com/twoday/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/twoday/sfgpetclinic/bootstrap/DataLoader.java
@@ -26,35 +26,30 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mr. First");
         owner1.setLastName("Owner");
         ownerService.save(owner1);
         System.out.println("Loaded Owner: " + owner1.getFirstName() + " " + owner1.getLastName());
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mr. Second");
         owner2.setLastName("Owner");
         ownerService.save(owner2);
         System.out.println("Loaded Owner: " + owner2.getFirstName() + " " + owner2.getLastName());
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Dr. First");
         vet1.setLastName("Vet");
         vetService.save(vet1);
         System.out.println("Loaded Vet: " + vet1.getFirstName() + " " + vet1.getLastName());
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Dr. Second");
         vet2.setLastName("Vet");
         vetService.save(vet2);
         System.out.println("Loaded Vet: " + vet2.getFirstName() + " " + vet2.getLastName());
 
         Pet pet1 = new Pet();
-        pet1.setId(1L);
         pet1.setName("Doggy");
         pet1.setOwner(owner1);
         petService.save(pet1);
