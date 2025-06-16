f9615c7c4703aa9ed30f96b5c4b5adda7418ac7d
Lauri Heinonen <lauri.heinonen@twoday.com>
2025-06-16 12:52:51 +0300

    controllers send data to index

--- pet-clinic-web/src/main/java/com/twoday/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/twoday/sfgpetclinic/controllers/PetController.java
@@ -1,14 +1,23 @@
 package com.twoday.sfgpetclinic.controllers;
 
+import com.twoday.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pets")
 @Controller
 public class PetController {
 
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listPets() {
+    public String listPets(Model model) {
+        model.addAttribute("pets", petService.findAll());
         return "pets";
     }
 
